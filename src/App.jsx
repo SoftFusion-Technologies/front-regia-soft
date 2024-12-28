021ffe6a08a4f67757336b64851c931c8f514088
Benjamin <goosta19802@gmail.com>
2024-12-28 11:32:13 -0300

    Se agrega react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
-    <>
-      <Home />
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Definir la ruta para la página de inicio */}
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
 import FeaturedProducts from './FeaturedProducts';
 import InfoSection from '../Components/InfoSection';
@@ -7,7 +6,6 @@ import ContactForm from '../Components/ContactForm';
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <Hero />
       <div className="mt-20">
         <FeaturedProducts />
